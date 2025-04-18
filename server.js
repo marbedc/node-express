aac5a5c1eb8268c194cf10350ce5e829d5a94048
marbedc <marbellacarrasco324@gmail.com>
2025-04-17 19:00:47 -0700

    updating js file

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
-// server.js
 const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// add any necessary code you'd want to!
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-//root
+// Home route
 app.get('/', (req, res) => {
-    // your nav bar is on index.html
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-    //res.sendFile(path.join(__dirname, 'index.html'))
-})
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-
-// Route for Climate Crisis
+// Redirect routes
 app.get("/climate-crisis", (req, res) => {
     res.redirect("https://github.com/whichcat/Save-the-Polar-Bears");
 });
-
-// Route for Typesetting
 app.get("/typesetting", (req, res) => {
     res.redirect("https://github.com/marbedc/typesetting");
 });
-
-// Route for Four Algorithms
 app.get("/four-algorithms", (req, res) => {
     res.redirect("https://github.com/marbedc/four-sorting-algo-phase1");
 });
 
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
